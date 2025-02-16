50bf043a333268a57565d46b21aaa256ace8e585
Gerald Byrket <gerald@gbsw.net>
2025-02-16 21:01:18 +0000

    WIP Link

---
+++ src/Form/LinkType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Link;
+use App\Entity\LinkCollection;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LinkType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('baseUrl')
+            ->add('uri')
+            ->add('active')
+            ->add('allowSharing')
+            ->add('linkCollection', EntityType::class, [
+                'class' => LinkCollection::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Link::class,
+        ]);
+    }
+}
