e85bd429548d6e8799f332c8a68f731dd379f49a
Gerald Byrket <gerald@gbsw.net>
2025-02-16 14:09:21 +0000

    Have this working down to collections

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
   namespace App\Controller;
 
   use App\Repository\CategoryRepository;
+  use App\Repository\LinkCollectionRepository;
   use App\Repository\OrganizationRepository;
   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
   use Symfony\Component\HttpFoundation\Request;
@@ -14,6 +15,7 @@
     public function __construct(
       private readonly OrganizationRepository $organizationRepository,
       private readonly CategoryRepository $categoryRepository,
+      private readonly LinkCollectionRepository $linkCollectionRepository,
     )
     {
     }
@@ -23,12 +25,53 @@
     {
       $activeOrgId = $request->query->get('orgId');
       $activeCatId = $request->query->get('catId');
+      $activeCollectionId = $request->query->get('collectionId');
       $orgs = $this->organizationRepository->getMyOrganizations();
-      $activeOrg = $this->organizationRepository->findOneById($activeOrgId);
+      $activeOrg = null;
+      $activeCategory = null;
+      $activeCollection = null;
+
+      if(! $activeOrgId && isset($orgs[0])) {
+        $activeOrgId = $orgs[0]->getId();
+      }
+
+      if($activeOrgId) {
+        $activeOrg = $this->organizationRepository->findOneById($activeOrgId);
+      }
+
+      if(! $activeCatId && $activeOrg) {
+        foreach ($activeOrg->getCategories() as $category) {
+          if(! $activeCatId) {
+            $activeCatId = $category->getId();
+          }
+        }
+      }
+      // get Active Category data
+      if($activeCatId) {
+        $activeCategory = $this->categoryRepository->findOneById($activeCatId);
+      }
+
+      if(! $activeCollectionId && $activeCategory) {
+        foreach ($activeCategory->getLinkCollections() as $collectionLink) {
+          if(! $activeCollectionId) {
+            $activeCollectionId = $collectionLink->getId();
+          }
+        }
+      }
+
+      // get Active Collection data
+      if($activeCollectionId) {
+        $activeCollection = $this->linkCollectionRepository->findOneById($activeCollectionId);
+      }
+
       return $this->render('home/index.html.twig', [
         'organizations' => $orgs,
-        'activeOrg' => (! $activeOrg && isset($orgs[0])) ? $orgs[0] : $activeOrg,
+        'activeOrg' => $activeOrg,
+        'activeCategory' => $activeCategory,
+        'activeCollection' => $activeCollection,
+        'activeOrgId' => $activeOrgId,
         'activeCatId' => $activeCatId,
+        'activeCollectionId' => $activeCollectionId,
       ]);
     }
 
