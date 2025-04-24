0aca9e72cba62741acc08c18b719959dd6949b83
Gerald Byrket <gerald@gbsw.net>
2025-04-24 21:23:54 +0000

    Added Copy Functionality

---
+++ assets/js/tabs.js
@@ -0,0 +1,38 @@
+function getPass(linkId)
+{
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
+
+        xhttp.open("GET", `/ajax/${linkId}/getLinkPass`, true);
+
+        xhttp.onload = function() {
+            console.log(this.responseText);
+            resolve(this.responseText);
+        }
+
+        xhttp.onerror = function() {
+            // Network error
+            console.error('Network Error');
+            reject();
+        };
+
+        xhttp.send();
+    });
+}
+
+async function copyPass(linkId)
+{
+    let pass = await getPass(linkId);
+    await navigator.clipboard.writeText(pass);
+}
+
+async function showPass(linkId) {
+    let pass = await getPass(linkId);
+    let eleId = "pass-" + linkId;
+    document.getElementById(eleId).innerHTML = `/ <a href="javascript:void(0);" onclick="hidePass(${linkId});">${pass}</a>`;
+}
+
+function hidePass(linkId) {
+    let eleId = "pass-" + linkId;
+    document.getElementById(eleId).innerHTML = `/ <a href="javascript:void(0);" onclick="showPass(${linkId});">show</a>`;
+}
